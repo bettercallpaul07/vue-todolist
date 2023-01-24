8dd9c4f4d832a135adc02aa147f50efc4945aac9
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-01-24 17:51:30 +0100

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        newTodo: "",
+        tasks: [
+            {
+                text: 'Fare la spesa',
+                done : false,
+            },
+            {
+                text: 'Lavare i piatti',
+                done : false,
+            },
+            {
+                text: 'Andare in palestra',
+                done : false,
+            },
+            {
+                text: 'Pulire la polvere',
+                done : true,
+            },
+            {
+                text: 'Comprare una casa',
+                done : false,
+            },
+          ],
+      }
+    },
+
+    methods: {
+
+        removeTodo: function(index) {
+            this.todos.splice(index,1);
+        },
+
+        addTodo: function() {
+
+            if(this.newTodo != "" && this.newTodo.length > 3) {
+
+            this.tasks.push({
+                text: this.newTodo,
+                done: false
+            })
+            this.newTodo = "";
+        }
+    }
+
+    }
+
+  }).mount('#app');
